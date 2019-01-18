d2dcf5b9e34a0b25e8a77a3edcf64bde0399aed6
Dheeresha <dheeresha.s@gmail.com>
2019-01-18 17:01:16 +0530

    Updates login route

--- app.js
+++ app.js
@@ -29,11 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/users',
-                                   failureRedirect: '/' })
-);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
+var passport = require('../libs/passport');
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
+
+router.post("/login", passport.authenticate("local", {  successRedirect: "/users",
+                                                        failureRedirect: "/" })
+);
 
 module.exports = router;
