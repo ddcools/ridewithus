a765fa68ed6ab2cf131c1b30d68a1fa17c745adf
Dheeresha <dheeresha.s@gmail.com>
2019-01-22 15:24:00 +0530

    Adds route to register user
    Adds bcrypt to deal with normal passwords

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,21 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models/index");
+var bcrypt = require("bcrypt");
 
 router.get("/", function(req, res, next) {
+  if (req.user) {
+    console.log(req.user);
+  } else {
+    console.log("user not yet logger in");
+  }
   res.send("respond with a resource");
 });
 
 router.post("/register", function(req, res) {
-  req.checkBody("username", "Username is required").notEmpty();
+  req.checkBody("email", "Email is required").notEmpty();
+  req.checkBody("firstname", "Firstname is required").notEmpty();
+  req.checkBody("lastname", "Lastname is required").notEmpty();
   req.checkBody("password", "Password is required").notEmpty();
 
   var errors = req.validationErrors();
@@ -17,7 +25,20 @@ router.post("/register", function(req, res) {
     res.status(422).json({ errors: errors });
   } else {
     req.session.success = true;
-    res.redirect("/");
+    var salt = bcrypt.genSaltSync(10);
+    var encryptedPassword = bcrypt.hashSync(req.body.email, salt);
+    db.User.create({
+      firstName: req.body.firstname,
+      lastName: req.body.lastname,
+      email: req.body.email,
+      password: encryptedPassword
+    })
+      .then(function(user) {
+        res.json({ user: user });
+      })
+      .catch(function(error) {
+        res.status(500).json({ error: error });
+      });
   }
 });
 
